428a2b99c24d7bc2ee3f2fa310b6e9ab93d3d305
Ram1528 <ramyaramalingam022@gmail.com>
2023-05-09 22:50:01 +0530

    first time

---
+++ src/test/java/com/git/Example.java
@@ -0,0 +1,17 @@
+package com.git;
+
+public class Example {
+
+         private void meth1() {
+System.out.println("1");
+		}
+         private void meth2() {
+        	 System.out.println("2");
+
+		}private void meth3() {
+			System.out.println("3");
+
+		}
+	}
+
+
