2ffd406aa056c67ed72733df44f9a71266d6d810
ramya1528 <ramyaramkb@gmail.com>
2023-05-10 20:18:53 +0530

    Shiva codes

--- src/test/java/com/git/Example.java
+++ src/test/java/com/git/Example.java
@@ -10,6 +10,13 @@ System.out.println("1");
 
 		}private void meth3() {
 			System.out.println("3");
+		}
+		private void meth6() {
+			System.out.println("6");
+
+		}
+		private void meth7() {
+			System.out.println("7");
 
 		}
 	}
