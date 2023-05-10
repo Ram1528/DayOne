9bf30e3d8b499f9566016092ae7a75aad7113eb2
Ram1528 <ramyaramalingam022@gmail.com>
2023-05-10 21:36:01 +0530

    aathu code1

--- src/test/java/com/git/Example.java
+++ src/test/java/com/git/Example.java
@@ -11,6 +11,14 @@ System.out.println("1");
 		}private void meth3() {
 			System.out.println("3");
 				}
+		private void methaathu() {
+			System.out.println("4");
+
+		}
+		private void methaathu2() {
+			System.out.println("5");
+
+		}
 		
 	}
 
