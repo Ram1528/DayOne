5ac2e24be0b93f43a9ac6071ac8867e917d16ec8
Ram1528 <ramyaramalingam022@gmail.com>
2023-05-11 13:37:42 +0530

    ram code1

--- src/test/java/com/git/Example.java
+++ src/test/java/com/git/Example.java
@@ -2,29 +2,38 @@ package com.git;
 
 public class Example {
 
-         private void meth1() {
-System.out.println("1");
-		}
-         private void meth2() {
-        	 System.out.println("2");
-		}
-         private void meth3() {
-			System.out.println("3");
-		}
-		private void meth6() {
-			System.out.println("6");
-		}
-		private void meth7() {
-			System.out.println("7");
-		}
-		private void methaathu() {
-			System.out.println("4");
-		}
-		private void methaathu2() {
-			System.out.println("5");
-
-		}
-		
+	private void meth1() {
+		System.out.println("1");
 	}
 
+	private void meth2() {
+		System.out.println("2");
+	}
 
+	private void meth3() {
+		System.out.println("3");
+	}
+
+	private void meth6() {
+		System.out.println("6");
+	}
+
+	private void meth7() {
+		System.out.println("7");
+	}
+
+	private void methaathu() {
+		System.out.println("4");
+	}
+
+	private void methaathu2() {
+		System.out.println("5");
+	}
+	private void methram1() {
+		System.out.println("8");
+	}
+	private void methram2() {
+		System.out.println("9");
+	}
+
+}
